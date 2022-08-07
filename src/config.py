efab703ecd54496ecd77aeca53cd2b90a8e547f5
Amir <nightspy2012@gmail.com>
2022-08-07 18:40:32 +0400

    updated config

---
+++ src/config.py
@@ -0,0 +1,42 @@
+import dataclasses
+from dataclasses import dataclass
+
+import yaml
+
+
+@dataclass
+class DatasetConfig:
+    coco_categories_map: dict
+    coco_train_annotations_url: str
+    coco_archive_train_filename: str
+    coco_archive_val_filename: str
+    raw_files_folder: str
+
+@dataclass
+class BaseConfig:
+    random_seed: int
+    dataset: DatasetConfig
+
+
+def __dataclass_from_dict(klass, d):
+    try:
+        fieldtypes = {f.name: f.type for f in dataclasses.fields(klass)}
+        return klass(**{f: __dataclass_from_dict(fieldtypes[f], d[f]) for f in d})
+    except:
+        return d  # Not a dataclass field
+
+
+
+def get_config_from_dvc() -> BaseConfig:
+    import dvc.api
+
+    params = dvc.api.params_show()
+    config = __dataclass_from_dict(BaseConfig, params)
+    return config
+
+
+def get_config_from_yaml(yaml_path: str) -> BaseConfig:
+    with open(yaml_path, "r") as file:
+        yaml_dict = yaml.safe_load(file)
+        config = __dataclass_from_dict(BaseConfig, yaml_dict)
+        return config
